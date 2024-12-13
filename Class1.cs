0345f107fc3eea41f46d0a883dabca73ce44c156
Hrishav Tiwari <A00286173@mycambrian.ca>
2024-12-12 21:01:48 -0500

    Initial commit for NuGet package project

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace HrishavNugetPackageLibrary
+{
+    public class Printer
+    {
+        public void PrintScreen(string message)
+        {
+            Console.WriteLine($"Message from HrishavNugetPackageLibrary: {message}");
+        }
+    }
+}
